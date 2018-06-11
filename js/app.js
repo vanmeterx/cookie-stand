7504df05f0337127ef36a8b9937689c4cd4af5ff
Ben Van Meter <vanmeterx@gmail.com>
2018-06-11 14:35:40 -0500

    Added objects; literal

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 'use strict';
+
+//Monday Lab
+
+var pike = {
+  name: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3
+};
+
+var seaTac = {
+  name: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2
+};
+
+var seaCenter = {
+  name : 'Seattle Center',
+  minCust : 11,
+  maxCust: 38,
+  avgCookie: 3.7
+};
+
+var capHill = {
+  name: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3
+};
+
+var alki = {
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6
+};
+
