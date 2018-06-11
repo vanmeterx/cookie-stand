c297297c7d7fbc981e6c6ba6b9f6673dbe4df025
Ben Van Meter <vanmeterx@gmail.com>
2018-06-11 16:01:30 -0500

    random number generator is successful. thnx jess

--- js/app.js
+++ js/app.js
@@ -2,54 +2,58 @@
 
 //Monday Lab
 
-/* need to add this to each object i tihnk?
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-*/
 var pike = {
   name: '1st and Pike',
   minCust: 23,
   maxCust: 65,
   avgCookie: 6.3,
 
-  custPerHour: function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + min;
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
   }
 };
-console.log(pike);
 
 var seaTac = {
   name: 'SeaTac Airport',
   minCust: 3,
   maxCust: 24,
-  avgCookie: 1.2
+  avgCookie: 1.2,
+
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
 };
 
 var seaCenter = {
   name : 'Seattle Center',
   minCust : 11,
   maxCust: 38,
-  avgCookie: 3.7
+  avgCookie: 3.7,
+
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
 };
 
 var capHill = {
   name: 'Capitol Hill',
   minCust: 20,
   maxCust: 38,
-  avgCookie: 2.3
+  avgCookie: 2.3,
+
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
 };
 
 var alki = {
   name: 'Alki',
   minCust: 2,
   maxCust: 16,
-  avgCookie: 4.6
+  avgCookie: 4.6,
+
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
 };
 
