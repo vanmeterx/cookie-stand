96fa71a9cad90664b46071ede02ab8527be798d3
Ben Van Meter <vanmeterx@gmail.com>
2018-06-12 15:04:14 -0500

    Adding list data to sales page

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ var pike = {
 
   custPerHour: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-  }, 
-  
+  },
+
   //next section came from code review: Day 7
   cookiesBought: function(){
     for(var i = 0; i < 15; i++){
@@ -84,7 +84,7 @@ var alki = {
   custPerHour: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
   },
-  
+
   cookiesBought: function(){
     for(var i = 0; i < 15; i++){
       this.hourlyArray[i] = Math.floor(this.custPerHour() * this.avgCookie);
@@ -92,3 +92,35 @@ var alki = {
   },
 };
 
+function simlulate() {
+  pike.custPerHour();
+  pike.cookiesBought();
+  seaTac.custPerHour();
+  seaTac.cookiesBought();
+  seaCenter.custPerHour();
+  seaCenter.cookiesBought();
+  capHill.custPerHour();
+  capHill.cookiesBought();
+  alki.custPerHour();
+  alki.cookiesBought();
+}
+
+var hours=['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm',
+  '3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
+
+function storeData(location,id){
+  var locationlist=document.getElementById(id);
+  for(var liIndex=0; liIndex<location.hourlyArray.length; liIndex++){
+    var li=document.createElement('li');
+    var listString=hours[liIndex]+': '+location.hourlyArray[liIndex]+' cookies.';
+    li.textContent=listString;
+    locationlist.appendChild(li);
+  }
+}
+
+simlulate();
+storeData(pike,'pike');
+storeData(seaTac,'seaTac');
+storeData(seaCenter,'seaCenter');
+storeData(capHill,'capHill');
+storeData(alki,'alki');
