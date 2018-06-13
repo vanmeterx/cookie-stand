fa4596019b5d376d4b41d7f6d4be7f01438bccbb
Ben Van Meter <vanmeterx@gmail.com>
2018-06-13 16:44:53 -0500

    started working on table layout with a copy/paste of found js, and started a quick sketch to determine how to set up a table in javascript.

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ var hours=['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:
 function simulateAndDisplayStoreData(location,id){
   location.cookiesBought();
 
-  var locationlist=document.getElementById(id);
+  var locationlist = document.getElementById(id);
 
   var cookieTotal = 0;
 
@@ -49,11 +49,11 @@ function simulateAndDisplayStoreData(location,id){
     cookieTotal = cookieTotal + cookiesForThisHour;
     console.log({ cookiesForThisHour, cookieTotal});
 
-    var listString=hours[i] + ': ' + cookiesForThisHour + ' cookies.';
+    var listString = hours[i] + ': ' + cookiesForThisHour + ' cookies.';
 
     // Create <li> with that string, and add to <ul>
-    var li=document.createElement('li');
-    li.textContent=listString;
+    var li = document.createElement('li');
+    li.textContent = listString;
     locationlist.appendChild(li);
   }
 
@@ -70,3 +70,41 @@ simulateAndDisplayStoreData(seaTac,'seaTac');
 simulateAndDisplayStoreData(seaCenter,'seaCenter');
 simulateAndDisplayStoreData(capHill,'capHill');
 simulateAndDisplayStoreData(alki,'alki');
+
+/* data from table: MDNWebdocs
+
+function generate_table() {
+    // get the reference for the body
+    var body = document.getElementsByTagName("body")[0];
+   
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+   
+    // creating all cells
+    for (var i = 0; i < 2; i++) {
+      // creates a table row
+      var row = document.createElement("tr");
+   
+      for (var j = 0; j < 2; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        var cell = document.createElement("td");
+        var cellText = document.createTextNode("cell in row "+i+", column "+j);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+   
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+   
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
+  }
+  */
