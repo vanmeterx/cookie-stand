7a57091ceceed7c52d43e82afe1091a3ed6f7b02
Ben Van Meter <vanmeterx@gmail.com>
2018-06-20 13:51:48 -0500

    added table head row

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //Monday Lab
 //Tuesday
 
-function shop (name, minCust, maxCust, avgCookie) {
+function Shop (name, minCust, maxCust, avgCookie) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -13,33 +13,52 @@ function shop (name, minCust, maxCust, avgCookie) {
 
 //note: declared functions do not need ';' after the '}'
 
-shop.prototype.custPerHour = function() {
+Shop.prototype.custPerHour = function() {
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
 };
 
-shop.prototype.cookiesBought = function() {
+Shop.prototype.cookiesBought = function() {
   for(var i = 0; i < 15; i++){
     this.hourlyArray[i] = Math.floor(this.custPerHour() * this.avgCookie);
   }
 };
 
-var pike = new shop('1st and Pike', 23, 65, 6.3);
+var pike = new Shop('1st and Pike', 23, 65, 6.3);
 
-var seaTac = new shop('SeaTac Airport', 3, 24, 1.2);
+var seaTac = new Shop('SeaTac Airport', 3, 24, 1.2);
 
-var seaCenter = new shop('Seattle Center', 11, 38, 3.7);
+var seaCenter = new Shop('Seattle Center', 11, 38, 3.7);
 
-var capHill = new shop('Capitol Hill', 20, 38, 2.3);
+var capHill = new Shop('Capitol Hill', 20, 38, 2.3);
 
-var alki = new shop('Alki', 2, 16, 4.6);
+var alki = new Shop('Alki', 2, 16, 4.6);
 
 var hours=['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm',
   '3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
 
-function simulateAndDisplayStoreData(location,id){
-  location.cookiesBought();
+var thead = document.createElement('thead');
 
-  var salesTable = document.getElementById('salesData');
+var theadtr = document.createElement('tr');
+
+
+var salesTable = document.getElementById('salesData');
+
+salesTable.appendChild(thead);
+thead.appendChild(theadtr);
+
+
+for(var i = 0; i < hours.length; i++){
+  var businessHours = hours[i];
+
+  var th = document.createElement('th');
+  th.textContent = businessHours;
+  theadtr.appendChild(th);
+
+  console.log(businessHours);
+}
+
+function simulateAndDisplayStoreData(location,id){ //displays in the store
+  location.cookiesBought();
 
   var cookieTotal = 0;
 
